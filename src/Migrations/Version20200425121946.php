611aff0e59a7298cce147e177001c706d866edf2
Anton Kutsenko <a.kutsenko@bp8.pl>
2020-04-25 14:20:42 +0200

    обновил миграции, переделал шаблон табов для переводов

--- src/Migrations/Version20200425121946.php
+++ src/Migrations/Version20200425121946.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200425075546 extends AbstractMigration
+final class Version20200425121946 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,14 +22,20 @@ final class Version20200425075546 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE u_user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_FCB96C9EE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE dimensions_products (id INT AUTO_INCREMENT NOT NULL, dimension_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_EB2B4A77277428AD (dimension_id), INDEX IDX_EB2B4A774584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE dimension (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE dimension_product (dimension_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_6143C6AB277428AD (dimension_id), INDEX IDX_6143C6AB4584665A (product_id), PRIMARY KEY(dimension_id, product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, locale VARCHAR(5) NOT NULL, INDEX IDX_3F207042C2AC5D3 (translatable_id), UNIQUE INDEX category_translation_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE dimensions_products_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, price VARCHAR(255) NOT NULL, locale VARCHAR(5) NOT NULL, INDEX IDX_813F39902C2AC5D3 (translatable_id), UNIQUE INDEX dimensions_products_translation_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, locale VARCHAR(5) NOT NULL, INDEX IDX_1846DB702C2AC5D3 (translatable_id), UNIQUE INDEX product_translation_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE dimension_translation (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, locale VARCHAR(5) NOT NULL, INDEX IDX_F78431742C2AC5D3 (translatable_id), UNIQUE INDEX dimension_translation_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, price DOUBLE PRECISION NOT NULL, image VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, image VARCHAR(255) NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product_category (product_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_CDFC73564584665A (product_id), INDEX IDX_CDFC735612469DE2 (category_id), PRIMARY KEY(product_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE dimension_product ADD CONSTRAINT FK_6143C6AB277428AD FOREIGN KEY (dimension_id) REFERENCES dimension (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE dimension_product ADD CONSTRAINT FK_6143C6AB4584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE dimensions_products ADD CONSTRAINT FK_EB2B4A77277428AD FOREIGN KEY (dimension_id) REFERENCES dimension (id)');
+        $this->addSql('ALTER TABLE dimensions_products ADD CONSTRAINT FK_EB2B4A774584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE category_translation ADD CONSTRAINT FK_3F207042C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES category (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE dimensions_products_translation ADD CONSTRAINT FK_813F39902C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES dimensions_products (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE product_translation ADD CONSTRAINT FK_1846DB702C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES product (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE dimension_translation ADD CONSTRAINT FK_F78431742C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES dimension (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE product_category ADD CONSTRAINT FK_CDFC73564584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
@@ -41,16 +47,23 @@ final class Version20200425075546 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE dimension_product DROP FOREIGN KEY FK_6143C6AB277428AD');
+        $this->addSql('ALTER TABLE dimensions_products_translation DROP FOREIGN KEY FK_813F39902C2AC5D3');
+        $this->addSql('ALTER TABLE dimensions_products DROP FOREIGN KEY FK_EB2B4A77277428AD');
         $this->addSql('ALTER TABLE dimension_translation DROP FOREIGN KEY FK_F78431742C2AC5D3');
-        $this->addSql('ALTER TABLE dimension_product DROP FOREIGN KEY FK_6143C6AB4584665A');
+        $this->addSql('ALTER TABLE dimensions_products DROP FOREIGN KEY FK_EB2B4A774584665A');
         $this->addSql('ALTER TABLE product_translation DROP FOREIGN KEY FK_1846DB702C2AC5D3');
         $this->addSql('ALTER TABLE product_category DROP FOREIGN KEY FK_CDFC73564584665A');
+        $this->addSql('ALTER TABLE category_translation DROP FOREIGN KEY FK_3F207042C2AC5D3');
+        $this->addSql('ALTER TABLE product_category DROP FOREIGN KEY FK_CDFC735612469DE2');
+        $this->addSql('DROP TABLE u_user');
+        $this->addSql('DROP TABLE dimensions_products');
         $this->addSql('DROP TABLE dimension');
-        $this->addSql('DROP TABLE dimension_product');
+        $this->addSql('DROP TABLE category_translation');
+        $this->addSql('DROP TABLE dimensions_products_translation');
         $this->addSql('DROP TABLE product_translation');
         $this->addSql('DROP TABLE dimension_translation');
         $this->addSql('DROP TABLE product');
         $this->addSql('DROP TABLE product_category');
+        $this->addSql('DROP TABLE category');
     }
 }
