d5245ffe0378871fcdf73990d9b24a2948102659
Anton Kutsenko <a.kutsenko@bp8.pl>
2020-04-26 06:50:34 +0200

    Сделал переключение языков

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LandingController extends AbstractCrudController
 {
     /**
-     * @Route("/{_locale}/", name="landing_index")
+     * @Route("/", name="landing_index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('landing.html.twig');
+    }
+
+    /**
+     * @Route("/change-locale/{locale}", name="change_locale")
+     */
+    public function changeLocale(string $locale, Request $request): Response
+    {
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirectToRoute('landing_index');
     }
 }
